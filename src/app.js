20c8dd33d136bb6394dde167e11d29a9e6a38f5e
Laura Alinani <lalinani@eagles.bridgewater.edu>
2024-01-30 22:19:23 -0500

    updated commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors'); 
 const userRouter = require('./routers/user') 
 const studyGroupRouter = require('./routers/studyGroup') 
+const notificationRouter = require('./routers/notification')
 
 const app = express() 
 
@@ -15,6 +16,7 @@ app.use(function (req, res, next) {
 app.use(express.json()) 
 app.use(userRouter) 
 app.use(studyGroupRouter)
+app.use(notificationRouter)
 
 const port = process.env.PORT || 3000 
 app.listen(port, () => { 
