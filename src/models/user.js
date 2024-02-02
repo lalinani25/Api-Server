a59a30893eff8f4ea6bdd1a249e6bf8cbe90a2d8
Laura Alinani <lalinani@eagles.bridgewater.edu>
2024-02-02 14:24:45 -0500

    updated commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,24 @@
+const validator = require('validator')
+
+const bcrypt = require('bcrypt')
+
 const mongoose = require('mongoose') 
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({ 
     email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-      lowercase: true
-    },
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+          if (!validator.isEmail(value)) {
+            throw new Error('Email is invalid.')
+          }
+        }
+      },
     username: { 
         type: String,
         required: true, 
@@ -33,6 +42,30 @@ const userSchema = new Schema({
     tokens: [String],
     profile_pic: Buffer
 })
+
+userSchema.pre('save', async function(next) {
+  
+    const user = this
+    
+    if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+    }
+    
+    next()  // run the save() method
+  })
+
+userSchema.methods.toJSON = function(){
+const user = this
+  
+const userObject = user.toObject()
+
+delete userObject.password
+delete userObject.tokens
+delete userObject.email_verified
+delete userObject.__v
+
+return userObject
+}
 
 const User = mongoose.model('User', userSchema);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const User = require('../models/user')
+const { sendVerificationEmail } = require('../emails/account.js')
 
 const router = new express.Router()
 
 // Add a new user
 router.post('/user', async (req, res) => {
+ 
+  delete req.body.email_verified
+  delete req.body.tokens  
+
   const user = new User(req.body)
 
   try {
     await user.save()
+    sendVerificationEmail(user.email, user.username)
     res.status(201).send(user)
   } 
   catch(error) {
